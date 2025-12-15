6436b608a4a28e21987f360e29180ea042137073
Durin Tasondock <temgoua484@gmail.com>
2025-12-15 08:18:06 +0100

    Ajout de la gestion des modules ouverts dans le composant FormationProgram et encapsulation dans Suspense

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,29 @@
 'use client';
-import {  useState } from 'react';
+import {  Suspense, useState } from 'react';
 import { modules } from '../data';
 import Module from '@/components/module';
+import { useSearchParams } from 'next/navigation';
+
 const FormationProgram = () => {
   const [expandedModule, setExpandedModule] = useState<number | null>(null);
   const toggleModule = (index: number) => {
     setExpandedModule(expandedModule === index ? null : index);
   };
+  const openAllModules = useSearchParams().get('openAllModules');
+  console.log('openAllModules', openAllModules);
 
   return (
     <div className="space-y-4">
       {modules.map((module, index) => (
-        <Module module={module} key={index} toggleModule={toggleModule} expandedModule={expandedModule} />
+        <Module module={module} key={index} toggleModule={toggleModule} expandedModule={openAllModules === 'yes' ? module.semaine : expandedModule} />
       ))}
     </div>)
 };
 
-export default FormationProgram;
+const WrappedFormationProgram = () => {
+  return <Suspense>
+    <FormationProgram />
+  </Suspense>;
+}
+
+export default WrappedFormationProgram;
