5da16f658be53539e41d06b1ec90acaf4ff88c2c
Durin Tasondock <temgoua484@gmail.com>
2025-12-15 08:24:19 +0100

    expand all by default

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,29 +1,20 @@
 'use client';
-import {  Suspense, useState } from 'react';
+import {  useState } from 'react';
 import { modules } from '../data';
 import Module from '@/components/module';
-import { useSearchParams } from 'next/navigation';
 
 const FormationProgram = () => {
   const [expandedModule, setExpandedModule] = useState<number | null>(null);
   const toggleModule = (index: number) => {
     setExpandedModule(expandedModule === index ? null : index);
   };
-  const openAllModules = useSearchParams().get('openAllModules');
-  console.log('openAllModules', openAllModules);
 
   return (
     <div className="space-y-4">
       {modules.map((module, index) => (
-        <Module module={module} key={index} toggleModule={toggleModule} expandedModule={openAllModules === 'yes' ? module.semaine : expandedModule} />
+        <Module module={module} key={index} toggleModule={toggleModule} expandedModule={module.semaine} />
       ))}
     </div>)
 };
 
-const WrappedFormationProgram = () => {
-  return <Suspense>
-    <FormationProgram />
-  </Suspense>;
-}
-
-export default WrappedFormationProgram;
+export default FormationProgram;
